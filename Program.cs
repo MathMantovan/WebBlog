e26f3864be442f17e9601e5c2b7d668ff09fa03f
Matheus Mantovan <math123mantovan@gmail.com>
2026-04-09 18:11:40 -0300

    Start na aula de autenticacao

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 using Blog.Data;
+using Blog.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -8,7 +9,9 @@ builder.Services.AddControllers()
         options.SuppressModelStateInvalidFilter = true;
     });
 builder.Services.AddDbContext<BlogDataContext>();
-
+builder.Services.AddTransient<TokenService>(); //sempre que eu usar vai criar uma nova instancia
+//builder.Services.AddScoped(); //Vai criar por transação, ou seja, se tiver mais de uma requisição. se em uma requisição eu usar 4 metodos com o toke, eu reutilizo ele 4 vezes
+//builder.Services.AddSingleton(); //um por app, vai carregar na memoria e sempre vai utilizar ele independente das requisições
 var app = builder.Build();
 
 app.MapControllers();
